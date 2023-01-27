8e818cfcb28491a8da2437090e2102a596152256
termik88 <dirkser@mail.ru>
2023-01-27 16:36:54 +0500

    Первый этап

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,13 @@
 import io
 import streamlit as st
 from PIL import Image
+
+from tensorflow import keras
 import numpy as np
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+import cv2 as cv
 
 
 @st.cache(allow_output_mutation=True)
-def load_model():
-    return EfficientNetB0(weights='imagenet')
-
-
-def preprocess_image(img):
-    img = img.resize((224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
-
-
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
     if uploaded_file is not None:
@@ -29,21 +17,40 @@ def load_image():
     else:
         return None
 
+def load_model():
+    return keras.models.load_model('model_v2')
 
-def print_predictions(preds):
-    classes = decode_predictions(preds, top=3)[0]
-    for cl in classes:
-        st.write(cl[1], cl[2])
+
+def preprocessor(data):
+    temp = []
+    img = cv.imread(data)
+    if type(img) == np.ndarray:
+        img1 = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+        img2 = cv.blur(img1,(3,3))
+        temp.append(cv.resize(img2,(32,32)))
+        scaled = np.array(temp)/255
+    return scaled.reshape(scaled.shape[0], scaled.shape[1], scaled.shape[2], 1)
+
+
+def get_answer(predict):
+    def get_aswer(value, predict):
+        return value + ' Predict: ' + str(max(predict[0])*100) + "%"
+
+    pred = np.argmax(predict)
+    
+    if pred == 0: return get_aswer('Bacterial pneumonia', predict)
+    elif pred == 1: return get_aswer('Normal', predict)
+    elif pred == 2: return get_aswer('Bitus pnrumonia', predict)
 
 
 model = load_model()
 
 
-st.title('Новая улучшенная классификации изображений в облаке Streamlit')
+st.title('Диагностика заболеваемости Пневмонией II ст. по результатам флюрографии ')
 img = load_image()
 result = st.button('Распознать изображение')
 if result:
-    x = preprocess_image(img)
-    preds = model.predict(x)
+    img_scaler = preprocessor(img)
+    pred = model.predict(img_scaler)
     st.write('**Результаты распознавания:**')
-    print_predictions(preds)
+    st.write(get_answer(pred))
