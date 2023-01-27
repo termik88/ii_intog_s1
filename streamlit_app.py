2a036df9cb95b4c939ba795171572dc4bdf2ec02
termik88 <dirkser@mail.ru>
2023-01-28 02:33:26 +0500

    local - good

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-import io
 import streamlit as st
 from PIL import Image
 
@@ -8,49 +7,63 @@ import cv2 as cv
 
 
 @st.cache(allow_output_mutation=True)
-def load_image():
-    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
-    if uploaded_file is not None:
-        image_data = uploaded_file.getvalue()
-        st.image(image_data)
-        return Image.open(io.BytesIO(image_data))
-    else:
-        return None
-
 def load_model():
     return keras.models.load_model('model_v2')
 
 
-def preprocessor(data):
+def preprocessor(img_file_bytes):
     temp = []
-    img = cv.imread(data)
-    if type(img) == np.ndarray:
-        img1 = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-        img2 = cv.blur(img1,(3,3))
-        temp.append(cv.resize(img2,(32,32)))
+    opencv_image = cv.imdecode(img_file_bytes, 1)
+    if type(opencv_image) == np.ndarray:
+        opencv_image1 = cv.cvtColor(opencv_image,cv.COLOR_BGR2GRAY)
+        opencv_image2 = cv.blur(opencv_image1,(3,3))
+        temp.append(cv.resize(opencv_image2,(32,32)))
         scaled = np.array(temp)/255
-    return scaled.reshape(scaled.shape[0], scaled.shape[1], scaled.shape[2], 1)
+        tenser_img = scaled.reshape(scaled.shape[0], scaled.shape[1], scaled.shape[2], 1)
+    return tenser_img
 
 
-def get_answer(predict):
-    def get_aswer(value, predict):
-        return value + ' Predict: ' + str(max(predict[0])*100) + "%"
+def load_image():
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
+    if uploaded_file is not None:
+        #отобразить загруженную картинку на странице
+        st.image(uploaded_file.getvalue())
+        #Конвертация файла
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        return file_bytes
+    else:
+        return None
 
-    pred = np.argmax(predict)
+
+def get_dict_aswers(predict):
+    def get_aswer(predict):
+        return round(predict*100, 2)
+
+    dict = {}
     
-    if pred == 0: return get_aswer('Bacterial pneumonia', predict)
-    elif pred == 1: return get_aswer('Normal', predict)
-    elif pred == 2: return get_aswer('Bitus pnrumonia', predict)
+    dict['Бактериальная пневмония: '] = get_aswer(predict[0][0])
+    dict['Отрицательно: '] = get_aswer(predict[0][1])
+    dict['Вирус пневмонии: '] = get_aswer(predict[0][2])
+    return dict
+
+
+def get_answer(dict_answers):
+    #сортируем в обратном порядке
+    dict_answers_sort = sorted(dict_answers.items(), key=lambda x:x[1], reverse=True)
+    
+    for i in range(len(dict_answers_sort)):
+        st.write(dict_answers_sort[i][0], dict_answers_sort[i][1], '%')
 
 
 model = load_model()
 
 
 st.title('Диагностика заболеваемости Пневмонией II ст. по результатам флюрографии ')
-img = load_image()
+img_file_bytes = load_image()
 result = st.button('Распознать изображение')
 if result:
-    img_scaler = preprocessor(img)
-    pred = model.predict(img_scaler)
-    st.write('**Результаты распознавания:**')
-    st.write(get_answer(pred))
+    img_scaler = preprocessor(img_file_bytes)
+    predict = model.predict(img_scaler)
+    st.write('**Вероятность диагноза:**')
+    dict_answers = get_dict_aswers(predict)
+    get_answer(dict_answers)
